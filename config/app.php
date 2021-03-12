970d325d14b53c151196b10f34bc96f3529be97a
Chess254 <lae2006a@yahoo.com>
2021-03-12 12:23:50 +0300

    cleaning

--- config/app.php
+++ config/app.php
@@ -171,7 +171,7 @@ return [
          */
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
-        Barryvdh\Cors\ServiceProvider::class,
+        // Barryvdh\Cors\ServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
