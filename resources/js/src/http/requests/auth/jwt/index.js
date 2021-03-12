da68702c2e09193d06c1b8f6a0a73ac3ee888f65
Chess254 <lae2006a@yahoo.com>
2021-03-12 17:54:16 +0300

    cleaning

--- resources/js/src/http/requests/auth/jwt/index.js
+++ resources/js/src/http/requests/auth/jwt/index.js
@@ -14,36 +14,36 @@ function addSubscriber(callback) {
 }
 
 export default {
-  // init() {
-  //   axios.interceptors.response.use(function (response) {
-  //     return response
-  //   }, function (error) {
-  //     // const { config, response: { status } } = error
-  //     const { config, response } = error
-  //     const originalRequest = config
+  init() {
+    axios.interceptors.response.use(function (response) {
+      return response
+    }, function (error) {
+      // const { config, response: { status } } = error
+      const { config, response } = error
+      const originalRequest = config
 
-  //     // if (status === 401) {
-  //     if (response && response.status === 401) {
-  //       if (!isAlreadyFetchingAccessToken) {
-  //         isAlreadyFetchingAccessToken = true
-  //         store.dispatch("auth/fetchAccessToken")
-  //           .then((access_token) => {
-  //             isAlreadyFetchingAccessToken = false
-  //             onAccessTokenFetched(access_token)
-  //           })
-  //       }
+      // if (status === 401) {
+      if (response && response.status === 401) {
+        if (!isAlreadyFetchingAccessToken) {
+          isAlreadyFetchingAccessToken = true
+          store.dispatch("auth/fetchAccessToken")
+            .then((access_token) => {
+              isAlreadyFetchingAccessToken = false
+              onAccessTokenFetched(access_token)
+            })
+        }
 
-  //       const retryOriginalRequest = new Promise((resolve) => {
-  //         addSubscriber(access_token => {
-  //           originalRequest.headers.Authorization = 'Bearer ' + access_token
-  //           resolve(axios(originalRequest))
-  //         })
-  //       })
-  //       return retryOriginalRequest
-  //     }
-  //     return Promise.reject(error)
-  //   })
-  // },
+        const retryOriginalRequest = new Promise((resolve) => {
+          addSubscriber(access_token => {
+            originalRequest.headers.Authorization = 'Bearer ' + access_token
+            resolve(axios(originalRequest))
+          })
+        })
+        return retryOriginalRequest
+      }
+      return Promise.reject(error)
+    })
+  },
   login(email, pwd) {
     return axios.post("/api/auth/login", {email: email, password: pwd})
   },
