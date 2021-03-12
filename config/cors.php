2a2b7129b2e39027624b68abd8471c6ac71f4c73
Chess254 <lae2006a@yahoo.com>
2021-03-12 12:33:17 +0300

    cleaning

--- config/cors.php
+++ config/cors.php
@@ -21,40 +21,58 @@ return [
      * You can enable CORS for 1 or multiple paths.
      * Example: ['api/*']
      */
-    'paths' => ['*'],
+    // 'paths' => ['*'],
 
-    /*
-    * Matches the request method. `['*']` allows all methods.
-    */
-    'allowed_methods' => ['*'],
+    // /*
+    // * Matches the request method. `['*']` allows all methods.
+    // */
+    // 'allowed_methods' => ['*'],
 
-    /*
-     * Matches the request origin. `['*']` allows all origins. Wildcards can be used, eg `*.mydomain.com`
-     */
+    // /*
+    //  * Matches the request origin. `['*']` allows all origins. Wildcards can be used, eg `*.mydomain.com`
+    //  */
+    // 'allowed_origins' => ['*'],
+
+    // /*
+    //  * Patterns that can be used with `preg_match` to match the origin.
+    //  */
+    // 'allowed_origins_patterns' => [],
+
+    // /*
+    //  * Sets the Access-Control-Allow-Headers response header. `['*']` allows all headers.
+    //  */
+    // 'allowed_headers' => ['*'],
+
+    // /*
+    //  * Sets the Access-Control-Expose-Headers response header with these headers.
+    //  */
+    // 'exposed_headers' => [],
+
+    // /*
+    //  * Sets the Access-Control-Max-Age response header when > 0.
+    //  */
+    // 'max_age' => 0,
+
+    // /*
+    //  * Sets the Access-Control-Allow-Credentials header.
+    //  */
+    // 'supports_credentials' => false,
+
+
+
+    'paths' => ['api/*'],
+
+    'allowed_methods' => ['GET, POST, PUT, PATCH, DELETE, OPTIONS'],
+
     'allowed_origins' => ['*'],
 
-    /*
-     * Patterns that can be used with `preg_match` to match the origin.
-     */
     'allowed_origins_patterns' => [],
 
-    /*
-     * Sets the Access-Control-Allow-Headers response header. `['*']` allows all headers.
-     */
-    'allowed_headers' => ['*'],
+    'allowed_headers' => ['Authorization,Accept,Origin,DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Content-Range,Range'],
 
-    /*
-     * Sets the Access-Control-Expose-Headers response header with these headers.
-     */
     'exposed_headers' => [],
 
-    /*
-     * Sets the Access-Control-Max-Age response header when > 0.
-     */
     'max_age' => 0,
 
-    /*
-     * Sets the Access-Control-Allow-Credentials header.
-     */
     'supports_credentials' => false,
 ];
